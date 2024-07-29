41d8bf38a15bfda33db3e63f76f1c8a504d52996
Hasmiemie <macbook@192.168.1.103>
2024-07-29 21:17:21 +0700

    h1

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Webcam from "react-webcam";
 
 function Example() {
+    const [imagePreview, setImagePreview] = useState()
+    const webcamRef = React.useRef(null);
+    const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImagePreview(imageSrc)
+    },
+    [webcamRef]
+  );
     return (
         <div className="container">
+            <h1>hallo</h1>
             <div className="row justify-content-center">
-                <Webcam/>
+            <div className="col-4">
+            {imagePreview && (
+                <img src={imagePreview} alt="" />
+            )}
             </div>
+            <div className="col-4"></div>
+            <div className="col-4"></div>
+            </div>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+      />
+      <button onClick={capture}>Capture photo</button>
         </div>
     );
 }
